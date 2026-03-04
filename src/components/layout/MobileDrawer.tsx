0fc403c65b22322af81734b2f7c69c627894326b
Jam <punnaporn1423zy@gmail.com>
2026-03-04 11:31:26 +0700

    run format

--- src/components/layout/MobileDrawer.tsx
+++ src/components/layout/MobileDrawer.tsx
@@ -28,18 +28,20 @@ export default function MobileDrawer({
     <>
       {/* Overlay */}
       <div
-        className={`md:hidden fixed inset-0 z-40 bg-black/60 transition-opacity duration-300 ${isOpen
+        className={`md:hidden fixed inset-0 z-40 bg-black/60 transition-opacity duration-300 ${
+          isOpen
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
-          }`}
+        }`}
         onClick={onClose}
         aria-hidden="true"
       />
 
       {/* Drawer Panel */}
       <div
-        className={`md:hidden fixed top-0 left-0 z-50 h-full w-72 bg-[#0B1121] border-r-[1.35px] border-[#374151] flex flex-col transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
+        className={`md:hidden fixed top-0 left-0 z-50 h-full w-72 bg-[#0B1121] border-r-[1.35px] border-[#374151] flex flex-col transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
       >
         {/* Drawer Header */}
         <div className="flex items-center justify-between px-4 py-3 border-b border-[#374151] shrink-0">
